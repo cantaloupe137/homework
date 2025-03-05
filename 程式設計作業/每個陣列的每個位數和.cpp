bd6445619786f9ba0dc1d26a726d700b7907e4ff
quack <122710532+rearyR@users.noreply.github.com>
2025-03-05 10:04:02 +0800

    Add files via upload

---
+++ 程式設計作業/每個陣列的每個位數和.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int sumAllDigit(int n) {
+	int sum = 0;
+	while (n != 0) {
+		int first = n % 10;
+		sum += first;
+		n /= 10;
+	}
+	return sum;
+}
+
+
+int main() {
+	int n, sum1 = 0;
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < n; i++) {
+		sum1 += sumAllDigit(arr[i]);
+	}
+	cout << sumAllDigit(sum1);
+	delete[] arr;
+	return 0;
+}
