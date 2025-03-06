8bc3d1674b6dfc7df9c8242239365ef5eeb3fd9b
quack <122710532+rearyR@users.noreply.github.com>
2025-03-06 19:47:57 +0800

    Update 1. 每個陣列的每個位數和.cpp

--- 程設練習/陣列/1. 每個陣列的每個位數和.cpp
+++ 程設練習/陣列/1. 每個陣列的每個位數和.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 using namespace std;
 
+//處理每個數字的位數和
 int sumAllDigit(int n) {
 	int sum = 0;
 	while (n != 0) {
@@ -17,13 +18,19 @@ int main() {
 	int n, sum1 = 0;
 	cin >> n;
 	int* arr = new int[n];
+
+	//輸入陣列內容
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
+
+	//加總
 	for (int i = 0; i < n; i++) {
 		sum1 += sumAllDigit(arr[i]);
 	}
+	
 	cout << sumAllDigit(sum1);
+	
 	delete[] arr;
 	return 0;
 }
