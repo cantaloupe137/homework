9bda75d1bcbe0d092dd571e69e490e993469339b
quack <122710532+cantaloupe137@users.noreply.github.com>
2025-03-12 11:11:10 +0800

    Create 10. 最小的數.c

---
+++ 程設練習/指標/10. 最小的數.c
@@ -0,0 +1,29 @@
+// 求一大小為n的整數陣列中最小的數？
+#include <stdio.h>
+#include <stdlib.h>
+
+int smallestNumInArr(int *arr, int n)
+{
+    int small = *arr;
+    for (int i = 0; i < n; i++)
+    {
+        if (small > *(arr + i))
+        {
+            small = *(arr + i);
+        }
+    }
+    return small;
+}
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    int *arr = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", (arr + i));
+    }
+    printf("%d", smallestNumInArr(arr, n));
+    return 0;
+}
