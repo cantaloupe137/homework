2680553848b8c1cfb9ebaab1034dfbd6fb375578
quack <122710532+cantaloupe137@users.noreply.github.com>
2025-03-12 11:30:02 +0800

    Create 14. 是否嚴格遞減.c

---
+++ 程設練習/指標/14. 是否嚴格遞減.c
@@ -0,0 +1,35 @@
+// 求一大小為n的整數陣列中，所有元素之值是否為遞增？
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int if_is_strict_decreasing(int *arr, int n)
+{
+    bool check = true;
+    for (int i = 1; i < n; i++)
+    {
+        if (*(arr + i - 1) <= *(arr + i))
+        {
+            check = false;
+            break;
+        }
+    }
+    if (check)
+        return true;
+    return false;
+}
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    int *arr = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", (arr + i));
+    }
+    if (if_is_strict_decreasing(arr, n))
+        printf("%s", "yes");
+    else
+        printf("%s", "no");
+    return 0;
+}
