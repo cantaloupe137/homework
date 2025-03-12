1bc7d4c470b0475646f29abc4bd9a73e342a8486
quack <122710532+cantaloupe137@users.noreply.github.com>
2025-03-12 11:04:30 +0800

    Update 1. 整數陣列元素的位數和.c

--- 程設練習/指標/1. 整數陣列元素的位數和.c
+++ 程設練習/指標/1. 整數陣列元素的位數和.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int sum_all_element(int *arr, int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int num = *(arr + i);
+        sum += num;
+    }
+    return sum;
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int *arr = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("%d", sum_all_element(arr, n));
+    free(arr);
+    return 0;
+}
